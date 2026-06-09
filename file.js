0a4e283f7e0cda9fccd6a172b9542d70e8383fe6
tenesacamoralesjohnny-png <tenesacamoralesjohnny@nhsdlions.org>
2026-06-09 08:00:17 -0400

    Update file.js

--- file.js
+++ file.js
@@ -1,28 +1,38 @@
-$(document).ready(function){
-//anonymous function
-$("h1,h2").click(function){
-	$(this).css(backround-color","purple");
-)};
-$("h1,h2").mouseover(function){
-	$(this).css(backround-color","black");
-)};
-$("h1").mousedown(function){
-	$(this).css(backround-color","blue");
-)};
-$("h1").mousedown(function){
-	$(this).css(backround-color","red");
-)};
-$("h1").mouseup(function){
-	$(this).css(backround-color","orange");
-)};
-$("h1").mouseup(function){
-	$(this).css(backround-color","yellow");
-)};
-$("h1").mouseleave(function){
-	$(this).css(backround-color","purple");
-)};
-$("h3")click(function){
-	$(h2).unbind("mouseup");
-	$(h2).unbind("mousedown");
-	)};
-)};
+$(document).ready(function() {
+
+    // anonymous function
+
+    $("h1,h2").click(function() {
+        $(this).css("background-color", "purple");
+    });
+
+    $("h1,h2").mouseover(function() {
+        $(this).css("background-color", "black");
+    });
+
+    $("h1").mousedown(function() {
+        $(this).css("background-color", "blue");
+    });
+
+    $("h1").mousedown(function() {
+        $(this).css("background-color", "red");
+    });
+
+    $("h1").mouseup(function() {
+        $(this).css("background-color", "orange");
+    });
+
+    $("h1").mouseup(function() {
+        $(this).css("background-color", "yellow");
+    });
+
+    $("h1").mouseleave(function() {
+        $(this).css("background-color", "purple");
+    });
+
+    $("h3").click(function() {
+        $("h2").unbind("mouseup");
+        $("h2").unbind("mousedown");
+    });
+
+});
